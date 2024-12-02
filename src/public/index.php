f25d3d0f7b29cd265f4bc217351843315b96280b
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-02 02:22:53 -0500

    feat: create public base

---
+++ src/public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * ==========================================================
+ *  Sherpa Frameworks  --  Private Development Version
+ * ==========================================================
+ *
+ * @package    Sherpa
+ * @version    0.1-dev
+ * @author     belicfr
+ * @license    MIT
+ * @link       https://github.com/sherpa-php
+ *
+ * ==========================================================
+ */
+
+
+session_start();
