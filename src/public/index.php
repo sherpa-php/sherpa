43c1a30bd9016916ae97940a047b57b95d274999
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-03 01:41:46 -0500

    feat: implement index

--- src/public/index.php
+++ src/public/index.php
@@ -6,7 +6,7 @@
  * ==========================================================
  *
  * @package    Sherpa
- * @version    0.1-dev
+ * @version    0.x (PDV)
  * @author     belicfr
  * @license    MIT
  * @link       https://github.com/sherpa-php
@@ -15,4 +15,31 @@
  */
 
 
+use Dotenv\Dotenv;
+use Sherpa\Core\router\Request;
+use Sherpa\Core\router\Router;
+
 session_start();
+ob_start();
+
+const __ROOT__ = __DIR__ . "/../..";
+const __SRC__ = __DIR__ . "/..";
+
+require_once __ROOT__ . "/vendor/autoload.php";
+
+require_once __ROOT__ . "/vendor/sherpa/core/src/core/utils.php";
+
+Dotenv::createImmutable(__ROOT__)
+    ->load();
+
+
+/*
+ * Router Loading
+ */
+
+require_once __SRC__ . "/router/default.php";
+
+$request = new Request();
+Router::resolve($request);
+
+ob_end_flush();
