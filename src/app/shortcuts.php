2960f7e5be64cff37606d124f505a2f4c2b18ceb
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-03 18:02:33 -0500

    docs: add shortcuts' PHPdocs

--- src/app/shortcuts.php
+++ src/app/shortcuts.php
@@ -3,26 +3,46 @@
 use Sherpa\Core\views\SherpaEngine;
 use Sherpa\Core\views\SherpaRendering;
 
+/**
+ * @return string Views directory absolute path
+ */
 function views_path(): string
 {
     return __SRC__ . "/views";
 }
 
+/**
+ * @return string Controllers directory absolute path
+ */
 function controllers_path(): string
 {
     return __SRC__ . "/controllers";
 }
 
+/**
+ * @return string Public directory absolute path
+ */
 function public_path(): string
 {
     return __SRC__ . "/public";
 }
 
+/**
+ * @return string Public/assets directory absolute path
+ */
 function assets_path(): string
 {
     return public_path() . "/assets";
 }
 
+/**
+ * View rendering shortcut, using Sherpa Engine
+ *
+ * @param string $viewPath
+ * @param array $props
+ * @param string $title (optional) Page's title
+ * @return SherpaRendering
+ */
 function render(string $viewPath, array $props = [], string $title = ""): SherpaRendering
 {
     $viewPathSplit = explode(':', $viewPath, 2);
