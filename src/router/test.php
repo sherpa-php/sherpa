35597fc84c848f1d1a4035ae84b455b250fa1c81
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-13 05:11:24 -0500

    dev: add two test routes examples

--- src/router/test.php
+++ src/router/test.php
@@ -1,9 +1,19 @@
 <?php
 
 use Sherpa\Core\router\Router;
+use Sherpa\Sherpa\test\simple\SimpleTest;
 
 /*
  * Sherpa Test routes definition
  */
 
-Router::get("/test", fn () => test());
+Router::get("/test", function ()
+{
+    echo "<a href='"
+        . route("test.simple")
+        . "'>Let's test with Sherpa Test!</a>";
+});
+
+Router::get("/test/simple",
+            fn () => test(SimpleTest::class))
+      ->name("test.simple");
