aa5756056d0909bfc57db46337d82ae29827b7ee
belicfr <96206878+belicfr@users.noreply.github.com>
2025-03-21 02:47:53 -0400

    docs: add missing Auth's PHPdocs

--- src/app/core/authenticate/Auth.php
+++ src/app/core/authenticate/Auth.php
@@ -3,13 +3,26 @@
 namespace Sherpa\Sherpa\app\core\authenticate;
 
 use Sherpa\Core\core\Sherpa;
+use Sherpa\Core\security\Hash;
 use Sherpa\Core\sessions\Session;
 use Sherpa\Sherpa\models\User;
 
+/**
+ * Authenticate main class.
+ * <p>
+ *     Manage all authentication processes:
+ *     attempt to log in, retrieve user's id or object,
+ *     etc.
+ * </p>
+ */
 class Auth
 {
     public const string SESSION_KEY = "sherpaf_session_user_id";
 
+    /**
+     * @return bool If the current session
+     *              is logged to a user's account
+     */
     public static function check(): bool
     {
         $storedUserId = self::id();
@@ -24,11 +37,19 @@ class Auth
                       ->count();
     }
 
+    /**
+     * @return int|null User's account's id if logged;
+     *                  else, NULL
+     */
     public static function id(): ?int
     {
         return Sherpa::session(self::SESSION_KEY);
     }
 
+    /**
+     * @return User|null User's account if logged;
+     *                   else, NULL
+     */
     public static function user(): ?User
     {
         return Auth::check()
